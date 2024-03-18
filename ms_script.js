ee8a0ba194c9c79acdcb07513a24256d32fa736d
megsummmers <70597858+megsummmers@users.noreply.github.com>
2024-03-18 01:25:06 -0400

    Update: finished? prototype
    just need to add the timeline page and fill in the project section

--- ms_script.js
+++ ms_script.js
@@ -1,5 +1,5 @@
 //variables
-let page = 'home';
+let page = 1;
 
 // hide ground after scrolling down
 window.addEventListener("scroll", function(){
@@ -13,6 +13,25 @@ window.addEventListener("scroll", function(){
     }
 }, false);
 
+window.addEventListener("keydown", function(event){
+    if(this.scrollY <= 10){
+        switch(event.key){
+            case "ArrowLeft":
+                if(page > 1 && page <= 4){
+                    page -= 1;
+                    moveCircle(page);
+                }
+                break;
+            case "ArrowRight":
+                if(page >= 1 && page < 4){
+                    page += 1;
+                    moveCircle(page);
+                }
+                break;
+        }
+    }
+}, false);
+
 function moveCircle(pageNum){
     switch(pageNum){
         case 1:
@@ -21,7 +40,7 @@ function moveCircle(pageNum){
         document.getElementById("PortfolioSec").style.display = "none";
         document.getElementById("ProjectSec").style.display = "none";
         document.getElementById("AboutSec").style.display = "none";
-        page = 'home';
+        page = 1;
         break;
         case 2: 
         document.getElementById("circle").style.left = "30%";
@@ -29,7 +48,7 @@ function moveCircle(pageNum){
         document.getElementById("PortfolioSec").style.display = "inline";
         document.getElementById("ProjectSec").style.display = "none";
         document.getElementById("AboutSec").style.display = "none";
-        page = 'portfolio';
+        page = 2;
         break;
         case 3: 
         document.getElementById("circle").style.left = "60%";
@@ -37,7 +56,7 @@ function moveCircle(pageNum){
         document.getElementById("PortfolioSec").style.display = "none";
         document.getElementById("ProjectSec").style.display = "none";
         document.getElementById("AboutSec").style.display = "inline";
-        page = 'about';
+        page = 3;
         break;
         case 4: 
         document.getElementById("circle").style.left = "90%";
@@ -45,41 +64,42 @@ function moveCircle(pageNum){
         document.getElementById("PortfolioSec").style.display = "none";
         document.getElementById("ProjectSec").style.display = "inline";
         document.getElementById("AboutSec").style.display = "none";
-        page = 'project';
+        page = 4;
         break;
     }
 }
 
+function previewJump(type, num){
+    console.log(":)");
+    window.scrollTo (0, 1500);
+    changeProject(type, num);
+}
+
 function changeProject(type, num){
-    var page1;
-    var page2;
-    var page3;
+    var pages = [];
+
     if(type == 'Pf'){
-        page1 = document.getElementById("Pf1");
-        page2 = document.getElementById("Pf2");
-        page3 = document.getElementById("Pf3");
+        pages[0] = document.getElementById("Pf1");
+        pages[1] = document.getElementById("Pf2");
+        pages[2] = document.getElementById("Pf3");
+        pages[3] = document.getElementById("Pf4");
+        pages[4] = document.getElementById("Pf5");
     } else if(type == 'Ab'){
-        page1 = document.getElementById("Ab1");
-        page2 = document.getElementById("Ab2");
-        page3 = document.getElementById("Ab3");
+        pages[0] = document.getElementById("Ab1");
+        pages[1] = document.getElementById("Ab2");
+        pages[2] = document.getElementById("Ab3");
     } else if(type == 'Pr'){
-        page1 = document.getElementById("Pr1");
-        page2 = document.getElementById("Pr2");
-        page3 = document.getElementById("Pr3");
+        pages[0] = document.getElementById("Pr1");
+        pages[1] = document.getElementById("Pr2");
+        pages[2] = document.getElementById("Pr3");
     }
     
-    switch(num){
-        case 1: page1.style.display = "inline";
-        page2.style.display = "none";
-        page3.style.display = "none";
-        break;
-        case 2: page1.style.display = "none";
-        page2.style.display = "inline-block";
-        page3.style.display = "none";
-        break;
-        case 3: page1.style.display = "none";
-        page2.style.display = "none";
-        page3.style.display = "inline";
-        break;
+    for(var i=0; i < pages.length; i++){
+        if(i == num-1){
+            pages[i].style.display = "inline-block";
+        }else{
+            pages[i].style.display = "none";
+        }
     }
 }
+
