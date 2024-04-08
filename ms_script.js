a46c8fa4b8147f389dc114948f8029b3c106e106
megsummmers <70597858+megsummmers@users.noreply.github.com>
2024-04-08 15:17:35 -0400

    Revert changes?

--- ms_script.js
+++ ms_script.js
@@ -4,20 +4,7 @@ let num = 0;
 let currentNum = 1;
 const abtScroll = document.getElementById("AboutSec");
 
-function scrollH(){
-    console.log("1");
-    if(document.scrollLeft >= 0 && document.scrollLeft <= 2160){
-        console.log("1");
-    } else if(this.scrollLeft > 2160 && this.scrollLeft <= 2160*2){
-        console.log("2");
-    } else if(this.scrollX > 2160*2 && this.scrollX <= 2160*3){
-        console.log("3");
-    } else if(this.scrollX > 2160*3 && this.scrollX <= 2160*4){
-        console.log("4");
-    }
-}
-
-document.getElementById("AboutSec").addEventListener("keydown", function(){
+window.addEventListener("keydown", function(){
     console.log(":)");
     if(page != 1){
         if(this.scrollY <= 10){
@@ -36,29 +23,41 @@ document.getElementById("AboutSec").addEventListener("keydown", function(){
 }, false);
 
 function moveCircle(pageNum){
-    $(document).ready(function () {
-        var $horizontal = $('#horizontal');
-    
-        $(window).scroll(function () {
-            var s = $(this).scrollTop(),
-                d = $(document).height(),
-                c = $(this).height();
-    
-            scrollPercent = (s / (d - c));
-    
-            var position = (scrollPercent * ($(document).width() - $horizontal.width()));
-    
-            $horizontal.css({
-                'left': position
-            });
-        });
-    });
-
-        document.getElementById("circle").style.left = "1%";
-        document.getElementById("name").style.display = "contents";
-        document.getElementById("PortfolioSec").style.display = "none";
-        document.getElementById("ProjectSec").style.display = "none";
-        document.getElementById("AboutSec").style.display = "none";
+    switch(pageNum){
+        case 1:
+            document.getElementById("circle").style.left = "1%";
+            document.getElementById("name").style.display = "contents";
+            document.getElementById("AboutSec").style.display = "none";
+            document.getElementById("PortfolioSec").style.display = "none";
+            document.getElementById("ProjectSec").style.display = "none";
+            page = 1;
+            break;
+        case 2:
+            document.getElementById("circle").style.left = "30%";
+            document.getElementById("name").style.display = "none";
+            document.getElementById("AboutSec").style.display = "contents";
+            document.getElementById("PortfolioSec").style.display = "none";
+            document.getElementById("ProjectSec").style.display = "none";
+            page = 1;
+            break;
+        case 3:
+            document.getElementById("circle").style.left = "60%";
+            document.getElementById("name").style.display = "none";
+            document.getElementById("AboutSec").style.display = "none";
+            document.getElementById("PortfolioSec").style.display = "contents";
+            document.getElementById("ProjectSec").style.display = "none";
+            page = 1;
+            break;
+        case 4:
+            document.getElementById("circle").style.left = "90%";
+            document.getElementById("name").style.display = "none";
+            document.getElementById("AboutSec").style.display = "none";
+            document.getElementById("PortfolioSec").style.display = "none";
+            document.getElementById("ProjectSec").style.display = "contents";
+            page = 1;
+            break;  
+    }
+        
 }
 
 function previewJump(type, num){
